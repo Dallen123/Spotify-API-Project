eada588a76c411f80cc505a7c9f4be1143902518
Dallen <dallenleeyuxuan123@gmail.com>
2022-01-26 18:39:19 +0800

    layout

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Tracklist.css'
+
+export class Tracklist extends React.Component {
+    render(){
+        return (
+            <div class="TrackList">
+    {// You will add a map method that renders a set of Track components 
+      }</div>
+        )}}
+        
